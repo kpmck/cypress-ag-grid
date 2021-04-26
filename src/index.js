02da335cab7b6eda67ffe869acc558e3a474cf37
Kerry McKeever <kmckeever@globys.com>
2021-04-25 21:52:26 -0400

    Removing d.ts file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-/// <reference types="./index" />
-
 import {getAgGrid, sortColumnBy} from "./agGrid/agGridInteractions"
 import {validateTablePages, validateTableExactOrder, validateEmptyTable, validateTableRowSubset} from "./agGrid/agGridValidations"
 import {filterByCheckboxColumnMenu, filterBySearchTextColumnFloatingFilter, filterBySearchTextColumnMenu, toggleColumnFromSideBar} from "./agGrid/agGridInteractions";
+
+export * from "./agGrid/filterOperator.enum";
+export * from "./agGrid/menuTab.enum";
+export * from "./agGrid/sort.enum";
 
 Cypress.Commands.add('getAgGrid', { prevSubject: true }, getAgGrid);
 Cypress.Commands.add('agGridSortColumn', {prevSubject: true}, sortColumnBy);
